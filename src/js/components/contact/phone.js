9354d54063b1a6ebad7fb8573de1f33e91ecb1e6
Vlad <v.zhurabetmen@gmail.com>
2024-11-23 16:37:37 +0200

    Added fprmat script

--- src/js/components/contact/phone.js
+++ src/js/components/contact/phone.js
@@ -12,7 +12,6 @@ export const usePhone = () => {
         .then((data) => callback(data.country_code))
         .catch(() => callback('us'));
     },
-    utilsScript:
-      'https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js',
+    utilsScript: 'https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js',
   });
 };
