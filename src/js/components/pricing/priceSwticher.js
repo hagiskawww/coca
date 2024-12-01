15cfc06cca69e066776e042b5b8c747a16f1b83e
Vlad <v.zhurabetmen@gmail.com>
2024-12-02 00:12:00 +0200

    Add Pricing/ Hero

--- src/js/components/pricing/priceSwticher.js
+++ src/js/components/pricing/priceSwticher.js
@@ -1,44 +1,65 @@
 export const usePriceSwitcher = () => {
   const switcher = document.querySelector('[data-price="swticher"]');
   const starterPrice = document.querySelector('[data-price="starter"]');
+  const starterPriceYo = document.querySelector('[data-price="starteryo"]');
   const popularPrice = document.querySelector('[data-price="popular"]');
-  const entriesPrice = document.querySelector('[data-price="enterprise"]');
-
+  const popularPriceYo = document.querySelector('[data-price="popularyo"]');
+  const enterPrice = document.querySelector('[data-price="enterprise"]');
+  const enterPriceYo = document.querySelector('[data-price="enterpriseyo"]');
   const priceList = {
     starter: {
-      default: 120,
-      witchSale: 100,
+      default: `$` + 120,
+      witchSale: `$` + 100,
     },
     popular: {
-      default: 1680,
-      witchSale: 1400,
+      default: `$` + 1680,
+      witchSale: `$` + 1400,
     },
-    enterprise: {
-      default: 2520,
-      wtichSale: 2100,
+    enterprice: {
+      default: `$` + 2520,
+      witchSale: `$` + 2100,
+    },
+  };
+  const priceListYo = {
+    starterYo: {
+      default: `$` + 1440,
+      witchSale: `$` + 1200,
+    },
+    popularYo: {
+      default: `$` + 20160,
+      witchSale: `$` + 16800,
+    },
+    enterpriseYo: {
+      default: `$` + 30240,
+      witchSale: `$` + 25200,
     },
   };
 
-  const setPricesWitchSale = () => {
+  const setPriceWitchSale = () => {
     starterPrice.textContent = priceList.starter.witchSale;
+    starterPriceYo.textContent = priceListYo.starterYo.witchSale;
     popularPrice.textContent = priceList.popular.witchSale;
-    entriesPrice.textContent = priceList.enterprise.wtichSale;
+    popularPriceYo.textContent = priceListYo.popularYo.witchSale;
+    enterPrice.textContent = priceList.enterprice.witchSale;
+    enterPriceYo.textContent = priceListYo.enterpriseYo.witchSale;
   };
 
-  const setDefaultPrices = () => {
+  const setPrice = () => {
     starterPrice.textContent = priceList.starter.default;
+    starterPriceYo.textContent = priceListYo.starterYo.default;
     popularPrice.textContent = priceList.popular.default;
-    entriesPrice.textContent = priceList.enterprise.default;
+    popularPriceYo.textContent = priceListYo.popularYo.default;
+    enterPrice.textContent = priceList.enterprice.default;
+    enterPriceYo.textContent = priceListYo.enterpriseYo.default;
   };
 
   switcher.checked = true;
-  setPricesWitchSale();
 
   switcher.addEventListener('click', () => {
     if (switcher.checked) {
-      setPricesWitchSale();
+      setPriceWitchSale();
     } else {
-      setDefaultPrices();
+      setPrice();
     }
   });
 };
--- src/js/pricing.js
+++ src/js/pricing.js
@@ -1,10 +1,12 @@
-import '/scss/pricing.scss';
+import '../assets/scss/pricing.scss';
 
 // components
 import { useTheme } from './components/theme.js';
-import { useBurger } from './components/burger.js';
+import { headerScroll } from './components/headerScroll.js';
 import { usePriceSwitcher } from './components/pricing/priceSwticher.js';
+import { useBurger } from './components/burger.js';
 
 useTheme();
 useBurger();
 usePriceSwitcher();
+headerScroll();
