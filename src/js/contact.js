eb4256ebb049ba298c474ccaab9215373fbb8310
Vlad <v.zhurabetmen@gmail.com>
2024-12-12 12:34:53 +0200

    add form

--- src/js/contact.js
+++ src/js/contact.js
@@ -3,8 +3,12 @@ import '../assets/scss/contact.scss';
 // components
 import { useTheme } from './components/theme.js';
 import { useBurger } from './components/burger.js';
+import { headerScroll } from './components/headerScroll.js';
+import { usePhone } from './components/contact/phone.js';
 import { sliderPartners } from './components/partnersSlider.js';
 
 useTheme();
 useBurger();
+usePhone();
+headerScroll();
 sliderPartners();
